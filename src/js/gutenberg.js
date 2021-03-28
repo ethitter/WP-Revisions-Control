bd86687e08b90fe9ba3da06a53a1a17ca5cf3647
Erick Hitter <git-contrib@ethitter.com>
2021-03-27 20:24:09 -0700

    Begin purge button

--- src/js/gutenberg.js
+++ src/js/gutenberg.js
@@ -1,4 +1,4 @@
-const { TextControl } = require( '@wordpress/components' );
+const { Button, TextControl } = require( '@wordpress/components' );
 const { compose } = require( '@wordpress/compose' );
 const { withSelect, withDispatch } = require( '@wordpress/data' );
 const { PluginDocumentSettingPanel } = require( '@wordpress/edit-post' );
@@ -6,34 +6,58 @@ const { __ } = require( '@wordpress/i18n' );
 const { registerPlugin } = require( '@wordpress/plugins' );
 
 const metaKey = '_wp_rev_ctl_limit';
+const slug = 'wp-revisions-control';
 
-const Render = ( { limit, update } ) => (
+const Render = ( { limit, manualPurge, showPurgeButton, updateMeta } ) => (
 	<PluginDocumentSettingPanel
-		name="wp-revisions-control"
+		name={ slug }
 		title={ __( 'WP Revisions Control', 'wp_revisions_control' ) }
-		className="wp-revisions-control"
+		className={ slug }
 	>
 		<TextControl
-			label={ __( 'Number of revisions to retain.', 'wp_revisions_control' ) }
+			label={ __( 'Number of revisions to retain:', 'wp_revisions_control' ) }
+			help={ __( 'Leave blank to keep all revisions.', 'wp_revisions_control' ) }
 			value={ limit }
-			onChange={ update }
+			onChange={ updateMeta }
 		/>
+
+		{ showPurgeButton && (
+			<Button onClick={ manualPurge }>
+				{ __( 'Purge excess revisions', 'wp_revisions_control' ) }
+			</Button>
+		) }
+
 	</PluginDocumentSettingPanel>
 );
 
 const RevisionsControl = compose(
 	[
 		withSelect( ( select ) => {
-			const limit = select( 'core/editor' ).getEditedPostAttribute(
+			const {
+				getCurrentPostRevisionsCount,
+				getEditedPostAttribute,
+			} = select( 'core/editor' );
+
+			const count = getCurrentPostRevisionsCount();
+			const limit = getEditedPostAttribute(
 				'meta'
 			)[ metaKey ];
 
+			const showPurgeButton = Boolean( limit )
+				&& count > parseInt( limit );
+
 			return {
 				limit,
+				showPurgeButton,
 			};
 		} ),
-		withDispatch( ( dispatch ) => {
-			const update = ( value ) => {
+		withDispatch( ( dispatch, { limit } ) => {
+			const manualPurge = () => {
+				// TODO: reuse the existing Ajax endpoint?
+				console.log( 'Purging!', limit );
+			};
+
+			const updateMeta = ( value ) => {
 				dispatch( 'core/editor' ).editPost( {
 					meta: {
 						[ metaKey ]: value,
@@ -42,7 +66,8 @@ const RevisionsControl = compose(
 			};
 
 			return {
-				update,
+				manualPurge,
+				updateMeta,
 			};
 		} ),
 	]
